ae4124d23fdb2e82a3e524bdd917589783705e96
lejinyoung <lejinyoung333@gmail.com>
2022-01-30 17:57:10 +0900

    220130

---
+++ Study_220128_Eg4_3.java
@@ -0,0 +1,26 @@
+public class Study_220128_Eg4_3 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for(int i = 1; i<=10; i++) {
+            for(int j = 1; j<=i; j++) {
+                sum += j;
+            }
+        }
+        System.out.print(sum);
+    }
+}
+
+
+// class Study_220128_Eg4_3__2 {
+//    public static void main(String[] args) {
+//
+//        int sum = 0;
+//        for(int i = 1; i<=10; i++) {
+//            for(int j = 1; j<=i; j++) {
+//                sum += j;
+//            }
+//        }
+//        System.out.print(sum);
+//    }
+//}
